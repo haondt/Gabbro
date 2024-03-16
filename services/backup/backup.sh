950f22e701cbdc0966608116143ad8a08df8fc33
Noah Burghardt <burghardtnoah@gmail.com>
2024-03-15 18:24:26 -0600

    updated backup script

--- services/backup/backup.sh
+++ services/backup/backup.sh
@@ -2,11 +2,10 @@ backup_name="$1"
 backup_local_path="$2"
 
 random=$(xxd -l 8 -p /dev/urandom)
-ts=$(date +%s)
 
-target_file="${backup_name}_${ts}_${random}.tar.gz"
+target_file="${random}.tar.gz"
 target_path="/tmp/${target_file}"
-echo "backing up '${backup_local_path}' to '${target_path}'"
+echo "compressing '${backup_local_path}' to '${target_path}'"
 
 if [ -d "${backup_local_path}" ]; then
     tar -czf "${target_path}" --directory="${backup_local_path}" . 2>&1
@@ -14,9 +13,9 @@ else
     tar -czf "${target_path}" --directory=$(dirname "${backup_local_path}") "$(basename ${backup_local_path})"  2>&1
 fi
 
-echo "copying '${target_file}' to cloud"
+echo "copying '${backup_name}.tar.gz' to cloud"
 # bucket uses object versioning, so we can just overwrite any existing file
-gcloud storage cp "${target_path}" "gs://$GCP_BUCKET/"
+gcloud storage cp "${target_path}" "gs://${GCP_BUCKET}/${backup_name}.tar.gz"
 
 echo "deleting tmp file '${target_path}'"
 rm "${target_path}"
