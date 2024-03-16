b8733e84c7d40cf997ce3105f6825125b28b7340
Noah Burghardt <burghardtnoah@gmail.com>
2024-03-15 18:44:56 -0600

    tmp try to fix

--- services/backup/backup.sh
+++ services/backup/backup.sh
@@ -10,14 +10,18 @@ target_path="/tmp/${target_file}"
 echo "compressing '${backup_local_path}' to '${target_path}'"
 
 if [ -d "${backup_local_path}" ]; then
+    echo "${backup_local_path} is a directory"
     tar -czf "${target_path}" --directory="${backup_local_path}" . 2>&1
 else
+    echo "${backup_local_path} is a single file"
     tar -czf "${target_path}" --directory=$(dirname "${backup_local_path}") "$(basename ${backup_local_path})"  2>&1
 fi
 
 echo "copying '${backup_name}.tar.gz' to cloud"
 # bucket uses object versioning, so we can just overwrite any existing file
-gcloud storage cp "${target_path}" "gs://${GCP_BUCKET}/${backup_name}.tar.gz"
+#gcloud storage cp "${target_path}" "gs://${GCP_BUCKET}/${backup_name}.tar.gz"
 
-echo "deleting tmp file '${target_path}'"
-rm "${target_path}"
+#echo "deleting tmp file '${target_path}'"
+#rm "${target_path}"
+
+echo "finished backing up ${backup_name} successfully :)"
